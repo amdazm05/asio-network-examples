d788dcc288e5d948582373e42bd5ce72d715d4c7
amdazm05 <amdali1998@yahoo.com>
2023-01-17 16:52:55 +0500

    feat: removed annoying logging message , added keep alive flag option

--- src/tcp_server.cpp
+++ src/tcp_server.cpp
@@ -21,7 +21,8 @@ void Asio_TCP_Server::WriteToClient(char * buffer, size_t sizeofBuffer) noexcept
         asio::error_code es;
         try
         {
-            asio::write(listOfclients[0], asio::buffer(buffer, sizeofBuffer), es);
+            // listOfclients[0].non_blocking(isBlockingMode);
+            listOfclients[0].write_some(asio::buffer(buffer, sizeofBuffer), es);
             if (es == asio::error::broken_pipe)
             {
                 DeallocateConnection(es);
@@ -44,17 +45,18 @@ std::size_t  Asio_TCP_Server::ReadFromClient(char * buffer) noexcept
         asio::error_code es;
         try
         {
+            // listOfclients[0].non_blocking(isBlockingMode);
             receptionByteCount= listOfclients[0].read_some(asio::buffer(_receptionbuffer), es);
             if (es && es != asio::error::eof)
             {
-                isServerConnected =false;
+                if(listOfclients[0].is_open())
+                    isServerConnected =false;
                 listOfclients.pop_back();
-                throw std::runtime_error("TCP Server: Socket Closed");
+                throw std::runtime_error("TCP Server: Nothing to Read");
             }		
             buffer = _receptionbuffer.data();
             if(receptionByteCount>0)
             {
-                std::cout<<buffer<<std::endl;
             }
         }
         catch (const std::exception &e)
@@ -118,13 +120,17 @@ void Asio_TCP_Server::AcceptConnection() noexcept
 {
     try
     {
-        if((acceptor_.get()!=nullptr && !isServerConnected))
-        {
+        if((acceptor_.get()!=nullptr)  )
+        {   
                 socket_ = std::shared_ptr<asio::ip::tcp::tcp::socket>(new asio::ip::tcp::tcp::socket(*io_service));
                 try
                 {
                     asio::error_code accept_error;
+                    // ::system::error_code ec;
                     acceptor_->accept(*socket_);
+                    asio::error_code es;
+                    asio::socket_base::keep_alive option(true);
+                    socket_->set_option(option,es);
                     isServerConnected = true;
                     listOfclients.emplace_back(std::move(*socket_));
                     socket_.reset();
