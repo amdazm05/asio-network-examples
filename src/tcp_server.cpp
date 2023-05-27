307d040c09ee3afaabb417a3b7ebb63fd73c7fe2
amdazm05 <amdali1998@yahoo.com>
2023-05-27 11:54:29 +0500

    revert: some important stack considerations

--- src/tcp_server.cpp
+++ src/tcp_server.cpp
@@ -239,9 +239,9 @@ void Asio_TCP_Server::ListenForConnections()
     return;
 }
 
-std::shared_ptr<std::array<char , 1<<16>> Asio_TCP_Server::GetReadBufferPointer()
+char * Asio_TCP_Server::GetReadBufferPointer()
 {
-	return std::shared_ptr<std::array<char , 1<<16>>(&(_receptionbuffer));
+	return (_receptionbuffer.data());
 }
 
 void Asio_TCP_Server::SetServerDisconnectionTimeout(int seconds)
